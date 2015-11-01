ab4b68c6736939a78874062ab55e9540b57e689c
Ilya Metsler <imetsler@gmail.com>
2015-11-02 03:36:29 +0600

    js accordion and view bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//-------This is Sliiiiiider----------
+
 $(document).ready(function(){
     $("#range_slider").slider({values:[1000, 50000],
         min:0,
@@ -10,45 +12,89 @@ $(document).ready(function(){
         }});
 });
 
+
+//-------This is view changer------------
 var ViewStateChange = (function(){
 
-    var _previousClass = '';
+    var _previousClass = "";
 
-    var _changeState = function($this){
+    var _changeState = function($this) {
         var item = $this.closest(".main-content__view-item"),
-            view = item.data('view'),
-            listOfItems = $('.product__list'),
-            modificationPrefix = 'product__list_',
+            view = item.data("view"),
+            listOfItems = $("#products-list"),
+            modificationPrefix = "product__list_",
             classOfViewState = modificationPrefix + view;
 
-        if (_previousClass == '') {
-            _previousClass = listOfItems.attr('class');
+        if(_previousClass == "") {
+            _previousClass = listOfItems.attr("class");
         }
-        _changeActiveClass($this);
-        listOfItems.attr('class', _previousClass + ' ' + classOfViewState);
-    }
 
-    var _changeActiveClass = function ($this) {
-        $this
-            .closest(".main-content__view-item").addClass('active')
-            .siblings().removeClass('active');
+        _changeActiveClass($this);
+        listOfItems.attr("class", _previousClass + " " + classOfViewState);
+    };
+
+    var _changeActiveClass = function($this) {
+        $this.closest(".main-content__view-item").addClass('active').siblings().removeClass('active');
     }
 
     return {
-        init: function(){
-            $('.main-content__view-link').on('click', function(e){
+        init: function() {
+            $("main-content__view-link").on("click", function(e) {
                 e.preventDefault();
                 _changeState($(this));
             });
         }
     }
-});
+}());
+
+
+//-------------This is accordion-----------
+
+var Accordion = (function()  {
+
+    var _openSection = function($this) {
+        var container = $this.closest(".filter__item"),
+            content = container.find(".filter__content"),
+            otherContent = $this.closest(".sidebar__list").find(".filter__content");
+
+        if(!container.hasClass("active")) {
+            otherContent.slideUp().closest(".filter__item").removeClass("active");
+
+            container.addClass("active");
+            content.stop(true, true).slideDown();
+        } else {
+            container.removeClass("active");
+            content.stop(true, true).slideUp();
+        }
+
+
+    };
+
+    return {
+        init: function() {
+            $(".filter__title").on("click", function(e) {
+                e.preventDefault();
+                _openSection($(this));
+            });
+        }
+    }
+}());
 
 $(document).ready(function(){
+
+    if($(".filter").length) {
+        Accordeon.init();
+    }
+    ViewStateChange.init();
+
 
     if ($(".main-content__select").length) {
         $(".main-content__select").select2({
             minimumResultsForSearch: Infinity
         });
     }
+
+
+
 });
+
